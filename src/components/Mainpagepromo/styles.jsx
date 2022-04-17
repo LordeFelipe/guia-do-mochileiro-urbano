a2ee32c3f58419975ae253c4192694399f1aa963
jontexman <jpmdoliveira@gmail.com>
2022-04-17 18:17:48 -0300

    :sparkles: Pagina inicial feita e botao de like com contador e limitação de like/deslike por pessoa

---
+++ src/components/Mainpagepromo/styles.jsx
@@ -0,0 +1,65 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+
+    .product{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        border-radius: 60px;
+        border: 1px solid black;
+        width: 80vh;
+        height: 20vh;
+        background-color: #554D4D;
+        img {
+            border-radius: 100px;
+            height: 18vh;
+            width: 18vh;
+            object-fit: cover;
+            margin-top: 1%;
+
+        }
+        .info{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            margin-top: 1%;
+
+        }
+        .title{
+          font-size: 4vh;
+          font-weight: normal;
+        }
+        p{
+          font-size: 2vh;
+        }
+        .old-price{
+          text-decoration: line-through;
+        }
+        .buttons{
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            margin-top: 4%;
+            .details{
+                height: 5vh;
+                font-size: 1.5rem;
+                color: white;
+                background-color: green;
+                transition-duration: 0.1s ease-in-out;
+                cursor: pointer;
+                :hover{
+                    background-image: linear-gradient(to right, #0ba360, #3cba92, #30dd8a, #2bb673);
+                }
+            }
+        }
+        .like-deslike{
+            width: 12vh;
+            display: flex;
+            justify-content: space-between;
+        }
+    }
+
+
+`
--- src/pages/ProfilePage/styles.jsx
+++ src/pages/ProfilePage/styles.jsx
@@ -1,8 +1,61 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
+  height: 90vh;
   background-color: #CFCFCF;
-  h1 {
-    color: blue;
+  .prof-page{
+    display: flex;
+    flex-direction: row;
+    height: 100%;
+    width: 100%;
+    .profile{
+      display:flex;
+      flex-direction: column;
+      justify-content: space-around;
+      align-items: center;
+      width: 65vh;
+      height: 75vh;
+      margin-top: 5%;
+      border-radius: 1rem;
+      margin-left: 5%;
+      border: 2px solid black;
+      margin-right: 10rem;
+      img {
+        height: 60%;
+        width:75%;
+        
+      }
+      h1{
+        color: black;
+        text-align: center;
+
+      }
+      button{
+        width: 60%;
+        height: 8%;
+        font-size: larger;
+        background-color: #554D4D;
+        text-align: center;
+        color: white;
+        border-radius: 10px;
+        transition-duration: 0.5s;
+        cursor: pointer;
+        :hover{
+        box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
+        }
+      }
+      
+    }
+    .marketlist{
+      border: 2px solid black;
+      border-radius: 20px;
+      display: flex;
+      height: 75vh;
+      margin-top: 5%;;
+      flex-direction: column;
+      justify-content: space-evenly;
+      background-color: #C4C4C4;
+      
+    }
   }
 `
