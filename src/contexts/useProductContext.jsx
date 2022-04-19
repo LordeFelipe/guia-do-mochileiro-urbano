14955be1c4e7de0a494655d6f8751c03d4e92572
Felipe <felipevaz444@gmail.com>
2022-04-19 20:27:23 -0300

    :sparkles: Detalhes produto funcionando

--- src/contexts/useProductContext.jsx
+++ src/contexts/useProductContext.jsx
@@ -1,10 +1,30 @@
 import { createContext, useContext, useState } from "react";
+import img from "../assets/default.png"
 
 const ProductContext = createContext()
 
 const ContextProvider = ({children}) => {
 
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([
+    {
+      name: 'Alfaca Verde',
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ullamcorper vulputate habitasse nunc, sed. Cras fringilla amet ac dictum malesuada nunc mi. Erat luctus ac elit aliquet nunc pellentesque orci montes, dictum. Vitae, lorem euismod lobortis arcu senectus accumsan id ac.",
+      oldPrice: 15,
+      newPrice: 20,
+      likes: 53,
+      deslikes: 20,
+      img: img
+    },
+    {
+      name: 'Sabonete',
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ullamcorper vulputate habitasse nunc, sed. Cras fringilla amet ac dictum malesuada nunc mi. Erat luctus ac elit aliquet nunc pellentesque orci montes, dictum. Vitae, lorem euismod lobortis arcu senectus accumsan id ac.",
+      oldPrice: 150,
+      newPrice: 25,
+      likes: 150,
+      deslikes: 15,
+      img: img
+    },
+  ])
 
   const [markets, setMarkets] = useState([])
 
